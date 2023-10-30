7f85c09964a39e142b6a76a6d14d234a6ea8ad22
Martin Drlík <533091+martindrlik@users.noreply.github.com>
2023-10-30 23:03:52 +0100

    flag parsing simplified

--- rex/main.go
+++ rex/main.go
@@ -2,34 +2,48 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 
-	"github.com/martindrlik/flag"
 	"github.com/martindrlik/rex/rex/client"
 	"github.com/martindrlik/rex/rex/server"
 )
 
 var (
-	addr     = flag.String("addr", ":1234", "server address")
-	isServer = flag.Bool("server", false, "run as server")
-	name     = flag.String("name", "test", "name of relation")
-	jsonStr  = flag.String("json", "", "json representation of relation to insert")
+	serverFlagSet = flag.NewFlagSet("server", flag.ExitOnError)
+	clientFlagSet = flag.NewFlagSet("client", flag.ExitOnError)
+
+	addr string
+	name string
+	jstr string
 )
+
+func init() {
+	for _, fs := range []*flag.FlagSet{serverFlagSet, clientFlagSet} {
+		fs.StringVar(&addr, "addr", ":1234", "server address")
+		fs.StringVar(&addr, "a", ":1234", "server address (shorthand)")
+	}
+	for _, fs := range []*flag.FlagSet{clientFlagSet} {
+		fs.StringVar(&jstr, "json", "", "json representation of relation to insert")
+		fs.StringVar(&jstr, "j", "", "json representation of relation to insert (shorthand)")
+		fs.StringVar(&name, "name", "default", "name of relation")
+	}
+}
 
 var command = map[string]func() error{
 	"insert": func() error {
 		m := map[string]any{}
-		if err := json.Unmarshal([]byte(*jsonStr), &m); err != nil {
-			return fmt.Errorf("unmarshaling: %w: %q\n", err, *jsonStr)
+		if err := json.Unmarshal([]byte(jstr), &m); err != nil {
+			return fmt.Errorf("unmarshaling: %w: %q\n", err, jstr)
 		}
-		if err := client.Insert(*addr, *name, m); err != nil {
+		if err := client.Insert(addr, name, m); err != nil {
 			return fmt.Errorf("calling: %w\n", err)
 		}
 		return nil
 	},
 	"list": func() error {
-		ms, err := client.List(*addr, *name)
+		ms, err := client.List(addr, name)
 		if err != nil {
 			return fmt.Errorf("calling: %w\n", err)
 		}
@@ -41,17 +55,20 @@ var command = map[string]func() error{
 }
 
 func main() {
-	flag.Parse()
-	if *isServer {
-		panic(server.Listen(*addr))
+	cmd := os.Args[1]
+	switch cmd {
+	case "server":
+		serverFlagSet.Parse(os.Args[2:])
+		panic(server.Listen(addr))
+	case "insert", "list":
+		clientFlagSet.Parse(os.Args[2:])
 	}
-	cmdName := flag.Arg(0)
-	cmd, ok := command[cmdName]
+	h, ok := command[cmd]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmdName)
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
 		return
 	}
-	if err := cmd(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s failed: %v\n", cmdName, err)
+	if err := h(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s failed: %v\n", cmd, err)
 	}
 }
