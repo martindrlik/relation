f57854f754cafbe4be747de8652efd505f3d71c6
Martin Drlík <533091+martindrlik@users.noreply.github.com>
2024-02-24 21:28:14 +0100

    preserve schema order

--- union.go
+++ union.go
@@ -6,7 +6,7 @@ func Union(t1, t2 *Table) *Table {
 	if !schema.IsEqual(t1.Schema(), t2.Schema()) {
 		return &Table{}
 	}
-	tbl := NewTable(schema.Slice(t1.Schema())...)
+	tbl := NewTable(t1.SchemaInOrder()...)
 	add := func(t T) { tbl.Add(t) }
 	t1.forEach(add)
 	t2.forEach(add)
