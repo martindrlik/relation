f1d33a617057310b4ea5b6412c47a636bf95cb83
Martin Drlík <533091+martindrlik@users.noreply.github.com>
2024-03-15 22:12:08 +0100

    rex command improved

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/martindrlik/rex
 
-go 1.21.5
+go 1.22.1
 
 require golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
-/*
- */
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
-	"sort"
+	"slices"
 
 	"github.com/martindrlik/rex/box"
 	"github.com/martindrlik/rex/load"
@@ -14,86 +13,105 @@ import (
 	"golang.org/x/exp/maps"
 )
 
-var (
-	format = flag.String("format", "table", "output format: table, json")
-)
-
 func main() {
-	flag.Usage = usage
-	flag.Parse()
+	bind("union", "", func(a, b *table.Table) *table.Table { return a.Union(b) })
+	bind("natural-join", "", func(a, b *table.Table) *table.Table { return a.NaturalJoin(b) })
+	exec(parse(os.Args[1:]))
+}
 
-	rest := func(n int) []string {
-		return flag.Args()[n:]
-	}
-	switch flag.Arg(0) {
-	case "print":
-		print(loadTables(rest(1)...))
-	case "union":
-		union(loadTables(rest(1)...))
-	case "naturaljoin":
-		naturaljoin(loadTables(rest(1)...))
-	case "project":
-		project(loadTables(flag.Arg(1)), rest(2)...)
+func exec(op string, tables []*table.Table, attributes []string) {
+	func(fn func([]*table.Table) []*table.Table) {
+		for _, t := range fn(tables) {
+			project(t, attributes)
+		}
+	}(binaryOp(op))
+}
+
+func binaryOp(op string) func([]*table.Table) []*table.Table {
+	return aggr(func(a, b *table.Table) *table.Table {
+		if desc, ok := ops[op]; ok {
+			return desc.fn(a, b)
+		}
+		usage(fmt.Errorf("unknown op: %s", op))
+		panic("unreachable")
+	})
+}
+
+func aggr(fn func(a, b *table.Table) *table.Table) func([]*table.Table) []*table.Table {
+	return func(tables []*table.Table) []*table.Table {
+		result := tables[0]
+		for _, t := range tables[1:] {
+			result = fn(result, t)
+		}
+		return []*table.Table{result}
 	}
 }
 
-func usage() {
-	fmt.Println("usage:")
-	fmt.Println("	rex print [file ...]")
-	fmt.Println("	rex union [file ...]")
-	fmt.Println("	rex naturaljoin [file ...]")
-	fmt.Println("	rex project file [attribute ...]")
+func project(table *table.Table, attributes []string) {
+	if len(attributes) == 0 {
+		attributes = maps.Keys(table.Schema())
+		slices.Sort(attributes)
+	}
+	fmt.Println(box.Table(attributes, table.Project(attributes...).Tuples()...))
+}
+
+func parse(args []string) (op string, tables []*table.Table, attributes []string) {
+	if len(args) < 2 {
+		usage(errors.New("missing arguments"))
+	}
+	names := stringsFlag{}
+	fs := flag.NewFlagSet("", flag.ExitOnError)
+	fs.Var(&names, "t", "table file")
+	op = args[0]
+	fs.Parse(args[1:])
+	if len(names) == 0 {
+		usage(errors.New("missing table files: -t filename"))
+	}
+	attributes = fs.Args()
+	tables = func() []*table.Table {
+		tables, err := load.TableFiles(names...)
+		if err != nil {
+			usage(fmt.Errorf("loading table: %w", err))
+		}
+		return tables
+	}()
+	return
+}
+
+type stringsFlag []string
+
+func (s *stringsFlag) String() string {
+	return fmt.Sprint(*s)
+}
+
+func (s *stringsFlag) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
+
+func usage(err error) {
+	if err != nil {
+		fmt.Println("Error:")
+		fmt.Printf("	%v\n", err)
+	}
+	fmt.Println("Usage:")
+	fmt.Println("	rex <command> -t filename [-t filename ...] [attribute ...]")
+	fmt.Println("Commands:")
+	names := maps.Keys(ops)
+	slices.Sort(names)
+	for _, name := range names {
+		fmt.Printf("	%s: %s\n", name, ops[name].desc)
+	}
 	os.Exit(1)
 }
 
-func loadTables(names ...string) []*table.Table {
-	tables, err := load.TableFiles(names...)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	return tables
+type opDesc struct {
+	desc string
+	fn   func(a, b *table.Table) *table.Table
 }
 
-func print(tables []*table.Table, attributes ...string) {
-	json := func(t *table.Table) {
-		panic("not implemented")
-	}
-	for _, t := range tables {
-		if len(attributes) == 0 {
-			attributes = maps.Keys(t.Schema())
-			sort.Strings(attributes)
-		}
-		if *format == "json" {
-			json(t)
-		} else {
-			fmt.Println(box.Table(attributes, t.Tuples()...))
-		}
-	}
-}
+var ops = map[string]opDesc{}
 
-func union(tables []*table.Table) {
-	t := tables[0]
-	for _, u := range tables[1:] {
-		t = t.Union(u)
-	}
-	print(tableSlice(t))
-}
-
-func naturaljoin(tables []*table.Table) {
-	t := tables[0]
-	for _, u := range tables[1:] {
-		t = t.NaturalJoin(u)
-	}
-	print(tableSlice(t))
-}
-
-func project(tables []*table.Table, attributes ...string) {
-	for _, t := range tables {
-		print(tableSlice(t.Project(attributes...)))
-	}
-}
-
-func tableSlice(tables ...*table.Table) []*table.Table {
-	return tables
+func bind(name, desc string, fn func(a, b *table.Table) *table.Table) {
+	ops[name] = opDesc{desc, fn}
 }
